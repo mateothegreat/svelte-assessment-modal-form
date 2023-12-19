91c4c01de584e599b693d812794714e52deb6db5
Adam G. Emerson <adamgemerson@gmail.com>
2023-12-19 14:41:51 -0500

    Adding Shadcn and tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,60 @@
 <script lang="ts">
-    import { modal } from '../stores/modal'
-    import Modal from "../components/Modal.svelte";
+	import '../app.pcss';
+	import { modal } from '../stores/modal';
+	import Button from "$lib/components/ui/button/button.svelte";
+	import * as Dialog from "$lib/components/ui/dialog";
 
-    let open = false
-    modal.subscribe(value => {open = value.open})
-    const handleButtonClick = () => {
-        modal.update(value => {
-            return {
-                ...value,
-                open: !value.open
-            }
-        })
-        console.log(modal)
-    }
+	let user = '';
 
+	const handleInputChange = (e: any) => {
+		user = e.target.value;
+	}
+
+	const handleClose = () => {
+		modal.update((state) => {
+			return {
+				...state,
+				open: false
+			}
+		})
+	}
+	const handleSubmit = () => {
+		console.log("Added User")
+		modal.update((state) => {
+			return {
+				...state,
+				users: [...state.users, user]
+			}
+		})
+		user = ''
+	}
 </script>
 
-
-<h1>Svelte Demo - Global Modal</h1>
-<button on:click|preventDefault={handleButtonClick}>{open ? 'Close Modal' : 'Open Modal'}</button>
-<Modal />
-<ul>
-    <li>
-        <a href="/one">Users (Page One)</a>
-    </li>
-    <li>
-        <a href="/two">Two</a>
-    </li>
-</ul>
-<slot/>
+<div class="m-8">
+	<Dialog.Root>
+		<Dialog.Trigger>Open Modal</Dialog.Trigger>
+		<Dialog.Content>
+			<Dialog.Header>
+				<Dialog.Title>Add A User</Dialog.Title>
+				<Dialog.Description>
+					Add a user to the list.
+				</Dialog.Description>
+			</Dialog.Header>
+			<input id="user" on:change|preventDefault={handleInputChange} value={user} class="outline">
+			<Button on:click={handleSubmit}>Add User</Button>
+		</Dialog.Content>
+	</Dialog.Root>
+	<h1 class="text-xl my-4">Svelte Demo - Global Modal</h1>
+	<div class="my-4">
+		<h2 class="text-lg">Navigation</h2>
+		<ul class="flex flex-row gap-4 underline mb-4">
+			<li>
+				<a href="/one">Users (Page One)</a>
+			</li>
+			<li>
+				<a href="/two">Two</a>
+			</li>
+		</ul>
+	</div>
+	<slot />
+</div>
