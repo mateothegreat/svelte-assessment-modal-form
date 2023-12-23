a2e38e6b2b1b44f6bf7db359f0041567733a4a2a
moilu <moisesluna0390@gmail.com>
2023-12-23 10:56:17 -0800

    Adds config files and work template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+  </script>
+<div class="grid justify-center max-w-4xl gap-2 mx-4 text-center md:mx-auto md:max-w-md lg:max-w-lg xl:max-w-2xl">
+    <h1 class="mb-4 text-3xl font-bold md:text-4xl lg:text-5xl">Welcome to Moi's Svelte Form Assessment</h1>
+    <p class="text-base md:text-lg lg:text-xl">Click here to test the form 👇</p>
+    <Button>Go to form</Button>
+  </div>
+  
