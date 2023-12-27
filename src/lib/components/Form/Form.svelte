f245a7b1766e8ec712424ada6462604b0f6ffd73
moilu <moisesluna0390@gmail.com>
2023-12-26 21:45:24 -0800

    Adds new components for form

--- src/lib/components/Form/Form.svelte
+++ src/lib/components/Form/Form.svelte
@@ -2,8 +2,25 @@
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import * as Form from '$lib/components/ui/form';
+	import { Separator } from '$lib/components/ui/separator';
 
 	export let open = false;
+
+	const languages = [
+		{ value: 'English', label: 'English' },
+		{ value: 'Spanish', label: 'Spanish' },
+		{ value: 'Chinese', label: 'Chinese' },
+		{ value: 'Russian', label: 'Russian' },
+		{ value: 'Portuguese', label: 'Portuguese' },
+		{ value: 'Arabic', label: 'Arabic' },
+		{ value: 'Hindi', label: 'Hindi' },
+		{ value: 'French', label: 'French' }
+	];
+
+	const close = () => {
+		open = false;
+	};
+
 	onMount(() => {
 		const handleEsc = (event: KeyboardEvent) => {
 			if (event.key === 'Escape') {
@@ -21,12 +38,19 @@
 
 {#if open}
 	<div
-		transition:fade={{ delay: 250, duration: 300 }}
+		transition:fade={{ delay: 100, duration: 300 }}
 		class="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 transition-opacity duration-300 ease-in-out"
 	>
-		<div class="rounded bg-white p-4 text-left shadow-lg">
+		<div class="relative max-w-[500px] rounded bg-white p-4 text-left shadow-lg">
+			<button class="absolute right-4 top-4 text-gray-600" on:click={close}>&times;</button>
+
+			<h2 class="mb-2 text-2xl font-bold">Create Camera</h2>
+			<p class="mb-4 text-gray-600">
+				Locations are used to group related cameras together. You can have as many locations and
+				cameras as you need.
+			</p>
 			<Form.Root let:config>
-				<div class="flex gap-4">
+				<div class="flex gap-4 pt-4">
 					<div class="w-3/5">
 						<Form.Field {config} name="cameraname">
 							<Form.Item>
@@ -40,19 +64,29 @@
 						<Form.Field {config} name="location">
 							<Form.Item>
 								<Form.Label>Location</Form.Label>
-								<Form.FormNativeSelect />
+								<Form.Select>
+									<Form.SelectTrigger placeholder="Select language" />
+									<Form.SelectContent>
+										{#each languages as lang}
+											<Form.SelectItem value={lang.value}>{lang.label}</Form.SelectItem>
+										{/each}
+									</Form.SelectContent>
+								</Form.Select>
 								<Form.Validation />
 							</Form.Item>
 						</Form.Field>
 					</div>
 				</div>
-				<Form.Field {config} name="description">
-					<Form.Item>
-						<Form.FormTextarea placeholder="Camera description/notes (Optional)" />
-						<Form.Validation />
-					</Form.Item>
-				</Form.Field>
-				<div class="flex gap-4">
+				<div class="py-4">
+					<Form.Field {config} name="description">
+						<Form.Item>
+							<Form.FormTextarea placeholder="Camera description/notes (Optional)" />
+							<Form.Validation />
+						</Form.Item>
+					</Form.Field>
+				</div>
+				<Separator />
+				<div class="flex gap-4 py-4">
 					<div class="w-4/5">
 						<Form.Field {config} name="address">
 							<Form.Item>
@@ -72,22 +106,24 @@
 						</Form.Field>
 					</div>
 				</div>
+				<Separator />
 				<Form.Field {config} name="email">
-					<Form.Item>
-						<Form.Label>Security emails</Form.Label>
+					<Form.Item class="mt-4 flex justify-between rounded border-2 border-slate-100 p-4">
+						<div>
+							<Form.Label>Security emails</Form.Label>
+							<Form.Description
+								>Receive emails about your account activity and security.</Form.Description
+							>
+						</div>
 						<Form.FormSwitch />
-						<Form.Description
-							>Receive emails about your account activity and security.</Form.Description
-						>
 						<Form.Validation />
 					</Form.Item>
 				</Form.Field>
-				<div class="text-right">
-					<Form.Button>Help</Form.Button>
+				<div class="flex justify-end gap-2 py-4">
+					<Form.Button variant="outline">Help</Form.Button>
 					<Form.Button>Save</Form.Button>
 				</div>
 			</Form.Root>
-			<!-- <button class="absolute top-4 right-4" on:click={() => change()}>&times;</button> -->
 		</div>
 	</div>
 {/if}
